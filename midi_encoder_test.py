7c74b1cced722d656977fad95cf350e064486a56
Zongyu <alexyin1012@gmail.com>
2018-01-31 16:59:26 +0000

    Clean unused files.
    Signed-off-by: Zongyu <alexyin1012@gmail.com>

--- midi_encoder_test.py
+++
@@ -1,6 +0,0 @@
-
-from music import note_sequence_io
-x = 0
-for i in note_sequence_io.note_sequence_record_iterator(
-        "/private/tmp/melody_rnn/sequence_examples/training_melodies.tfrecord"):
-    print(i)
